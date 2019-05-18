f91fdc7f28aa0f8f85f6d1a50cf2ac2692e3ec43
Bliker <erfan2333@gmail.com>
2019-05-18 13:48:01 +0800

    now cookie can configure by config file

--- diskcloud/blueprint/diskcloud/views/login.py
+++ diskcloud/blueprint/diskcloud/views/login.py
@@ -1,4 +1,4 @@
-from flask import request,redirect,render_template,url_for,make_response
+from flask import request,redirect,render_template,url_for,make_response,current_app
 
 from diskcloud.models.session import create_session,get_value_session,delete_session as del_session
 from diskcloud.models.valid import valid_user
@@ -16,7 +16,7 @@ def Login():
             if request.form.get('enable_cookie',False):
                 cookie_id = set_cookie_id(username)
                 if cookie_id:
-                    response.set_cookie('login_id',cookie_id,86400,domain='bliker.ga',path='/diskcloud',httponly=True,secure=True,samesite='Lax')
+                    response.set_cookie('login_id',cookie_id,86400,domain=current_app.config['SESSION_COOKIE_DOMAIN'],path=current_app.config['SESSION_COOKIE_PATH'],httponly=current_app.config['SESSION_COOKIE_HTTPONLY'],secure=current_app.config['SESSION_COOKIE_SECURE'],samesite=current_app.config['SESSION_COOKIE_SAMESITE'])
                 else:
                     return render_template('error.html',err_mes = 'Setting cooike failed')
             return response
@@ -27,7 +27,7 @@ def Login():
     if request.args.get('logout'):
         response = make_response(render_template('login.html'))
         if cookie_id:
-            response.delete_cookie('login_id')
+            response.delete_cookie('login_id',path=current_app.config['SESSION_COOKIE_PATH'],domain=current_app.config['SESSION_COOKIE_DOMAIN'])
         del_session('username')
         return response
 
