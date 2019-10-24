c93b542ef33b57a7a79a2608bc4705f4228b46d3
Bliker <erfan2333@gmail.com>
2019-10-24 18:11:16 +0800

    delete some unnecessary files

---
+++ diskcloud/instance.py
@@ -0,0 +1,4 @@
+from diskcloud import create_app
+
+# conf = 'path/to/yourconfig'
+app = create_app()
--- diskcloud/views/settings.py
+++
@@ -1,7 +0,0 @@
-from flask import current_app
-
-def Settings():
-    response_string = ''
-    for key,value in current_app.config.items():
-        response_string += key + ' : ' + str(value) + '<br>\n'
-    return response_string
